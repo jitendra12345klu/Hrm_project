a0c46e69fde889dd9213a084cadcddb8f583abed
Koyya Jitendra Kumar <100194309+jitendra12345klu@users.noreply.github.com>
2025-01-01 17:15:40 +0530

    Add files via upload

--- myStepsFile.ts
+++ myStepsFile.ts
@@ -3,35 +3,51 @@ import { fixture } from "../../hooks/pageFixture";
 import { LogInPage } from "../../pages/myPageFile";
 let loginpage : LogInPage;
 
-Given('User Navigate to LogIn Page', async function () {
+//navigate to url
+Given('User Navigate to {string}', async function (url:string) {
     loginpage = new LogInPage();
-    await loginpage.navigateToLoginPage();
+    await loginpage.navigateToLoginPage(url);
 });
+//console for log in page
 When('User is on LogIn Page', async function () {
     console.log(' User is on log in page');
 });
+
 When('User enters {string} and {string} Clicks on LogIn Button', async function (UserName:string,Password:string) {
     loginpage = new LogInPage();
     await loginpage.logInFunctionality(UserName,Password);
 });
+
 When('click on footer icons', async function () {
     console.log(' click on footer icons');
 });
+
 When('click on Forgot Password and enter {string}', async function (username:string) {
     loginpage = new LogInPage();
     await loginpage.forgetPasswordFunctionality(username);
 });
-
+//validate log in page elements
 Then('User should see LogIn Page Elements', async function () {
     loginpage = new LogInPage();
-    await loginpage.ValidateLogInPageElements();
+    await loginpage.ValidateLogInPageWebElements();
 });
 
 Then('User should redirect to respective pages', async function () {
     loginpage = new LogInPage();
     loginpage.logInPageFooterIcons();
 });
+
 Then('User should get reset password link', async function () {
     loginpage = new LogInPage();
     loginpage.forgetPasswordValidation();
+});
+
+Then('User should see Dashboard Elements', async function () {
+    loginpage = new LogInPage();
+    loginpage.ValidateDashboardPageWebElements();
+});
+
+Then('User should see Profile DropDown', async function () {
+    loginpage = new LogInPage();
+    loginpage.dashboardProfileDropDown();
 });
